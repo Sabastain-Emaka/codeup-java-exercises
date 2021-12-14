5ba93bed16504bed1379c39d3af8bdad5dc092c3
sabastain wakoyi <jevian@WAKOYIs-MacBook-Pro.local>
2021-12-14 12:21:26 -0600

    hello world

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+//        System.out.println("Hello World");
+
+//        System.out.print("Hello ");
+//        System.out.print("World ");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+}
