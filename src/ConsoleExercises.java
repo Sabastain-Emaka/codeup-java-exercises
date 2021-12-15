00c3f86c3a16fddbb144196a34fbc8f434647252
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2021-12-15 12:12:59 -0600

    ConsoleIO exercises

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -25,21 +25,27 @@ public class ConsoleExercises {
         // 2. Prompt a user to enter 3 words, and store each of them in a separate variable.
         // Then, display them back in the console, each on a newline.
 
-     System.out.println("Pleas, enter 3 words");
-     String firstWord = sc.next();
-     String secondWord = sc.next();
-     String thirdWord = sc.next();
-
-     System.out.print("Your 3 words are:\n" + firstWord + "\n" + secondWord +"\n" + thirdWord);
-     System.out.printf("%s%n%s%n%s%n", firstWord, secondWord, thirdWord);
+//     System.out.println("Pleas, enter 3 words");
+//     String firstWord = sc.next();
+//     String secondWord = sc.next();
+//     String thirdWord = sc.next();
+//
+//     System.out.print("Your 3 words are:\n" + firstWord + "\n" + secondWord +"\n" + thirdWord);
+//     System.out.printf("%s%n%s%n%s%n", firstWord, secondWord, thirdWord);
 
         //What happens if you enter less than 3 words?
+        // It still prints out all the words
         //What happens if you enter more than 3 words?
+        // It still prints out all the words
 
 // 3. Prompt a user to enter a sentence, then store that sentence in a String variable using the next method.
 // Then, display that sentence back to the user.
-//do you capture all the words?
+//
         System.out.println("Please enter a sentence");
+        String userSentence = sc.next();
+        System.out.print("Your sentence is:\n" + userSentence);
 
+        // do you capture all the words?
+        // No, only the first word is display
     }
 }
