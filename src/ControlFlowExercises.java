9a42972b31d02a4a4b4959ace9986cfdfec2e824
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2021-12-16 13:14:59 -0600

    controlflow exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
 
     public static void main(String[] args) {
@@ -101,6 +103,39 @@ public class ControlFlowExercises {
                 System.out.println(num);
             }
 
+        }
+        // // Exercise 3 - Display a table of powers.
+        ////
+        ////		-Prompt the user to enter an integer.
+        ////		-Display a table of squares and cubes from 1 to the value entered.
+        ////		-Ask if the user wants to continue.
+        ////		-Assume that the user will enter valid data.
+        ////		-Only continue if the user agrees to.
+        ////
+        //// 		Example Output
+        ////
+        ////		What number would you like to go up to? 5
+        ////
+        ////		Here is your table!
+        ////
+        ////				number | squared | cubed
+        ////				------ | ------- | -----
+        ////				1      | 1       | 1
+        ////				2      | 4       | 8
+        ////				3      | 9       | 27
+        ////				4      | 16      | 64
+        ////				5      | 25      | 125
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int num = in.nextInt();
+        in.nextLine();
+        System.out.println("\n Here is our table\n");
+        System.out.println("number | squared | cubed");
+        System.out.println("---- | ---- | ----");
+        for (int i = 1; i <= num; i += 1) {
+            System.out.printf("%");
         }
     }
 }
