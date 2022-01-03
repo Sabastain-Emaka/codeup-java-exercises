0340f00bac5819081f6b7a7526893f7da4c3c87e
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-03 11:42:56 -0600

    string exercises

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -7,6 +7,10 @@ public class StringExercise {
         String Message2 = "Check \"this\" out!, \"s inside of \"s!";
         System.out.println(Message2);
 
-        String Message3 = " "
+        String Message3 = " In windows, the main drive is usually C:\\ ";
+        System.out.println(Message3);
+
+        String Message4 = "I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\! ";
+        System.out.println(Message4);
     }
 }
