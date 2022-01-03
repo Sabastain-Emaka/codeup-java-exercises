97fc9f9a38dad7caaf43244c490df12567832bcc
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-03 11:52:11 -0600

    bob exercises

--- src/Bob.java
+++ src/Bob.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String KeepTalkingToBob = "y";
 
     }
 }
