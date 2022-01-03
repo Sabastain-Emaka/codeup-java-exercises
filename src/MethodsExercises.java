0a943b48d068462452b535fe1c17dc0591baed94
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-03 16:29:57 -0600

    Methods Exercises

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class MethodsExercises {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // 1 Basic Arithmetic Methods
+
+//    System.out.println(SumOf  2, 4);
+
+    }
+
 }
