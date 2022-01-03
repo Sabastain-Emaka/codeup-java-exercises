47a3fc9b1d372649fd3d1436ed2e9bea26ae458b
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-03 10:52:27 -0600

    string lec

--- src/StringsLec.java
+++ src/StringsLec.java
@@ -3,14 +3,23 @@ import java.util.Scanner;
 public class StringsLec {
     public static void main(String[] args) {
 
-        char d = 'd';
-        System.out.println(d == 'd');
+//        char d = 'd';
+//        System.out.println(d == 'd');
+//
+//        Scanner sc = new Scanner(System.in);
+//        String favoriteSoda = "Pepsi Wild Chery";
+//        System.out.println("Hey Jevian, what is your favorite soda?");
+//        String userSoda = sc.nextLine();
+//        System.out.printf("Jevian remembered his favorite soda, and typed it's name properly! %b", favoriteSoda.equals(userSoda));
+//        System.out.printf("Jevian remembered his favorite soda: %b", favoriteSoda.equalsIgnoreCase(userSoda));
 
-        Scanner sc = new Scanner(System.in);
-        String favoriteSoda = "Pepsi Wild Chery";
-        System.out.println("Hey Jevian, what is your favorite soda?");
-        String userSoda = sc.nextLine();
-        System.out.printf("Jevian remmered his favorite soda, and typed it's name properly! %b", favoriteSoda.equals(userSoda));
+
+        // compare the start and end of a string
+
+        String jevian = "Jevian";
+        String jev = "Jev";
+        char firstLetterOfDavid = jevian.charAt(0);
+
 
     }
 }
