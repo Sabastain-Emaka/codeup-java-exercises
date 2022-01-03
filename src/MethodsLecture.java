8ffa6b585423edaa6edfd54aac3e7132964eba55
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-03 15:08:17 -0600

    methods

---
+++ src/MethodsLecture.java
@@ -0,0 +1,21 @@
+public class MethodsLecture {
+
+
+    //Methods
+
+    /*
+    *A java method is a collection of statements that are grouped together to perform an operation.
+    * Methods can be defined with or without parameters, as well with or without a return type.
+     */
+
+    // public static returnType nameOfMethod() {}
+
+    public static int returnNum(int num) {
+        return  num;
+    }
+
+    public static void main(String[] args) {
+        returnNum(7);
+
+    }
+}
