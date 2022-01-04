b1360dc614190f30c514fd92189a2abd8f14fb85
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-03 19:30:54 -0600

    Methods Exercises

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -7,30 +7,48 @@ public class MethodsExercises {
 
         // 1 Basic Arithmetic Methods
 
-//System.out.println(SumOfNums  (2, 4));
-//System.out.println(SubtractNums  7, 9));
-//System.out.println(DivideNums  (2, 4));
-//System.out.println(modulus (7, 3));
-//System.out.println(product (4, 4));
+       // addition
+//    public static int addition(int a, int b) {
+//        return (a + b);
+//    }
+//
+//    //subtraction
+//    public static int subtraction(int a, int b) {
+//        return (a - b);
+//    }
+//
+//    //multiplication
+//    public static int multiplication(int a, int b) {
+//        return (a * b);
+//    }
+//
+//    //division
+//    public static int division(int a, int b) {
+//        return (a / b);
+//    }
+//
+//    //modulus
+//    public static int modulus(int a, int b) {
+//        return (a % b);
+//    }
 
-        public static int SumOfNums(int num1, int num2) {
-            return num1 + num2;
-        }
+        //bonus
+//    public static int multiply(int x, int y) {
+//        if (x == 0 || y == 0) {
+//            return 0;
+//        } else if (y > 0) {
+//            System.out.println("x " + x);
+//            System.out.println("y " + y);
+//
+//            int count = x + multiply(x, y - 1);
+//
+//            System.out.println("count " + count);
+//
+//            return count;
+//        }
+//        return multiply(x, y);
+//    }
 
-        public static int SubtractNums(int num1, int num2) {
-            return num1 - num2;
-        }
-
-        public static int DivideNums(int num1, int num2) {
-            return num1/num2;
-        }
-        public static int Modulus(int num1, int num2) {
-            return num1%num2;
-        }
-
-        public static int Product(int num1, int num2) {
-            return num1*num2;
-        }
     }
 
 }
