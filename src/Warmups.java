e4023bcc5ad0fc12154011456e8600e4ce1dc9e0
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-04 09:21:08 -0600

    warmup completed

--- src/Warmups.java
+++ src/Warmups.java
@@ -1,13 +1,24 @@
 import java.util.Scanner;
 
 public class Warmups {
+
+
+    public static String startDay(){
+        return "Brain Loading...Done";
+    }
+
     public  static void main(String[] args) {
 //        System.out.println("Java is statically typed, while Javascript is dynamically typed");
 
         // 12/17/2021
-        System.out.println("What did you eat for breakfast today?");
-        Scanner scanner = new Scanner(System.in);
-        String userBreakfast = scanner.nextLine();
-        System.out.printf("%s, That's nice food", userBreakfast);
+//        System.out.println("What did you eat for breakfast today?");
+//        Scanner scanner = new Scanner(System.in);
+//        String userBreakfast = scanner.nextLine();
+//        System.out.printf("%s, That's nice food", userBreakfast);
+
+
+            System.out.println(startDay());
+
+
     }
 }
