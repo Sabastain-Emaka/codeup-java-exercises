8248ac1f1836c89480eb24a7617d19f8f905851f
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-05 10:19:05 -0600

    Object Oriented Programming Lec

---
+++ src/Pearson.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class Person {
+    private String name;
+    private int age;
+    private int weight;
+    private int heightInCM;
+
+    public Person() {
+
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getHeightInCM() {
+        return heightInCM;
+    }
+
+    public void setHeightInCM(int heightInCM) {
+        this.heightInCM = heightInCM;
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person(String name, int age, int weight, int heightInCM) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.heightInCM = heightInCM;
+    }
+
+    String sayHello() {
+        return String.format("Hello from %s! They hope you are well!", name);
+    }
+
+    public String toString() {
+        return String.format("{%nname: %s;%nage: %d;%nweight: %d;%nheightInCM: %d;%n}", name, age, weight, heightInCM);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Person joe = new Person("Joe", 64, 350, 180);
+        System.out.println(joe);
+//        joe.name = "Joe";
+//        System.out.println(joe.sayHello());
+//        Person susan = new Person();
+//        susan.name = "Susan";
+//        System.out.println(susan.sayHello());
+//        System.out.println(joe);
+    }
+}
