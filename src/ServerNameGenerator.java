d446965427ab160dd9f57bee77ca8a74db24f743
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-06 17:10:47 -0600

    array exercises

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,3 +1,18 @@
 public class ServerNameGenerator {
 
+    public  static int randomWord(String[] arrays) {
+        return (int) Math.floor(Math.random() * arrays.length);
+    }
+
+    public static void main(String[] args) {
+        String[] adjectives = {"great", "excellent", "analyze", "effective", "above", "beyond", "champion", "assess","measure", "describe"};
+        String[] nouns = {"Jevi", "Mbande", "Ande", "Akwenko", "Ebuepka", "Berabe", "Ako", "Assa", "Buku","Akwaja" };
+
+        String adjective = adjectives[randomWord(adjectives)];
+        String noun = (nouns[randomWord(nouns)]);
+
+        System.out.printf("Your server name is:\n%s, \n%s", adjective, noun);
+    }
+
+
 }
