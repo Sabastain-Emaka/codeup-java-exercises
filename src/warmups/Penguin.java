569dea258281d561831e8ca807ef0a84b3d4184a
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-06 13:41:46 -0600

    warmups

--- src/warmups/Penguin.java
+++ src/warmups/Penguin.java
@@ -6,4 +6,18 @@ public class Penguin {
     private String species;
     private char sex;
 
+    public static boolean canFly() {
+        return false;
+    }
+
+    public static boolean areEvil(){
+        return true;
+    }
+
+    public Penguin(String name, int weight, String species, char sex) {
+        this.name = name;
+        this.weight = weight;
+        this.species = species;
+        this.sex = sex;
+    }
 }
