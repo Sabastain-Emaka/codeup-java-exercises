abfa1e384c63e9a8ae9f92a1ca4ac0bd7c27c5ea
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-07 15:52:45 -0600

    inheritance and polymorphism exe

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,32 +1,19 @@
 package shapes;
 
-public class Rectangle extends Quadrilateral {
+public class Rectangle {
 
-    public  Rectangle(double length, double width) {
+    public Rectangle(double length, double width) {
         super(length, width);
-
     }
 
     @Override
-    public  double grtPerimeter() {
+    public double getArea() {
+        return this.length * this.width;
+    }
+
+    @Override
+    public double getPerimeter() {
         return (2 * this.length) + (2 * this.width);
     }
-
-    @Override
-    public  double getArea () {
-        return this.width * this.length;
-    }
-
-    @Override
-    void setWidth(double width) {
-        this.width = width;
-    }
-
-    @Override
-    void setLength(double length){
-        this.length = length;
-    }
-
-
-
 }
+
