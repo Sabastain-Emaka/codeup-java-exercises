45392970cd4ae73b5513bfcf34d2e9d75869f2c0
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-07 10:08:20 -0600

    movie exe

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -1,6 +1,17 @@
 package movies;
 
-public class MoviesApplication {
+import util.Input;
+
+public class MoviesApplication extends MoviesArray {
+    static Input input = new Input();
+    static Movie[] movies = MoviesArray.findAll();
+
+    public static void main(String[] args) {
+        boolean userContinue = true;
+        do {
+            System.out.println("Which movie action do you want to take");
+        }
+    }
 
 
 }
