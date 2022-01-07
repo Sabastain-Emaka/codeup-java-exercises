1be58340511968c3de998765e828f6b610c3e37f
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-07 10:16:39 -0600

    movie exe

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -9,8 +9,10 @@ public class MoviesApplication extends MoviesArray {
     public static void main(String[] args) {
         boolean userContinue = true;
         do {
-            System.out.println("Which movie action do you want to take");
-        }
+            System.out.println("Which movie action do you want to take?\n\n0-exit\n1-view movies in the animated category\\3 - view movies in the drama category\\n4 - view movies in the horror category\\n5 - view movies in the sci-fi category\\n6 - add a new movie\\n");
+            System.out.println("Enter your choice: ");
+
+        }while (userContinue);
     }
 
 
