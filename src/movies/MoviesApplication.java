5332b3a4a53e04ffdb6d507c9f3646b3d935da22
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-07 11:12:33 -0600

    movie exe

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -12,6 +12,35 @@ public class MoviesApplication extends MoviesArray {
             System.out.println("Which movie action do you want to take?\n\n0-exit\n1-view movies in the animated category\\3 - view movies in the drama category\\n4 - view movies in the horror category\\n5 - view movies in the sci-fi category\\n6 - add a new movie\\n");
             System.out.println("Enter your choice: ");
 
+                int userInput = input.getInt(0,6);
+
+                if(userInput ==0) {
+                    System.out.println("Thanks, Good");
+                    System.exit(1);
+                } else if (userInput ==1) {
+                    for (Movie movie: movies) {
+                        System.out.println(movie.getCategory() + "--" + movie.getName());
+                    }
+                } else if(userInput ==2) {
+                    for (Movie movie : movies) {
+                        if(movie.getCategory().equals("animated")) {
+                            System.out.println(movie.getCategory() + "--" + movie.getName());
+                        }
+                    }
+                } else if (userInput == 3) {
+                    for (Movie movie : movies) {
+                        if (movie.getCategory().equals("drama")) {
+                            System.out.println(movie.getCategory() + " -- " + movie.getName());
+                        }
+                    }
+                } else if (userInput == 4) {
+                    for (Movie movie : movies) {
+                        if (movie.getCategory().equals("horror")) {
+                            System.out.println(movie.getCategory() + " -- " + movie.getName());
+                        }
+                    }
+                }
+
         }while (userContinue);
     }
 
