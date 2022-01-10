339c7a53f88d08e82d3ec609f1b2cd398f49ef65
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-10 12:15:30 -0600

    working on interfaces and abstract exercises

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,23 +1,58 @@
 package shapes;
 
-public class Rectangle {
-    protected double length;
-    protected double width;
+public class Rectangle extends  Quadrilateral implements  Measurable{
 
-    public Rectangle( double length,  double width) {
-        this.length = length;
-        this. width = width;
+
+    public Rectangle(double length, double width) {
+        super(length, width);
     }
 
-
-    public double getArea() {
-        return this.length * this.width;
+    @Override
+    public void set_length(double _length) {
+        this._length = _length;
     }
 
+    public double get_length() {
+        return this._length;
+    }
 
+    @Override
+    public void set_width(double _width) {
+        this._width = _width;
+    }
+
+    @Override
     public double getPerimeter() {
-        return (2 * this.length) + (2 * this.width);
+        return (2 * this._length) + (2 * this._width);
     }
+
+    @Override
+    public double getArea() {
+        return (this._length * this._width);
+    }
+
+
+
+
+
+    // Solution to exercise 1 on Inheritance and Polymorphism
+//    protected double length;
+//    protected double width;
+//
+//    public Rectangle( double length,  double width) {
+//        this.length = length;
+//        this. width = width;
+//    }
+//
+//
+//    public double getArea() {
+//        return this.length * this.width;
+//    }
+//
+//
+//    public double getPerimeter() {
+//        return (2 * this.length) + (2 * this.width);
+//    }
 
 }
 
