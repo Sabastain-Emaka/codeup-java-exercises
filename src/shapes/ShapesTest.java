4f83ee79be32e172e7f4b5eead8fc72b53139b6c
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-10 14:33:44 -0600

    completed the exercises on abstract and interfaces

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -3,15 +3,35 @@ package shapes;
 public class ShapesTest {
     public static void main(String[] args) {
 
-        Measurable myShape1 = new Square(6);
-        Measurable myShape2 = new Rectangle(3,5);
+        Measurable myShape1 = new Square(4.0);
+        Measurable myShape2  = new Rectangle(5, 10);
 
-        System.out.println(myShape1.getPerimeter());
-        System.out.println(myShape2.getPerimeter());
+        Rectangle r1 = new Rectangle(3,10);
+        r1.setLength(8);
 
-        Rectangle box1 = new Rectangle(4, 5);
-        System.out.println(box1.getPerimeter());
-        System.out.println(box1.getArea());
+        System.out.println("Square");
+        System.out.println("Area: " + myShape1.getArea());
+        System.out.println("Perimeter: " + myShape1.getPerimeter());
+        System.out.println("===================");
+
+        System.out.println("Rectangle");
+
+        System.out.println("Area: " + myShape2.getArea());
+        System.out.println("Perimeter: " + myShape2.getPerimeter());
+        System.out.println("===================");
+
+        System.out.println("Rectangle");
+        System.out.println("Area: " + r1.getArea());
+        System.out.println("Perimeter: " + r1.getPerimeter());
+
+
+
+
+        // Solutions to exercise 1 on inheritance and polymorphism
+
+//        Rectangle box1 = new Rectangle(4, 5);
+//        System.out.println(box1.getPerimeter());
+//        System.out.println(box1.getArea());
 
 //        Rectangle box2 = new Square(5);
 //        System.out.println(box2.getPerimeter());
