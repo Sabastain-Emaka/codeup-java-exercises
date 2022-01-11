6c9960a9816a776030a792d490d88d01323756f0
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-11 12:10:12 -0600

    working on grades exercise

---
+++ src/grades/Student.java
@@ -0,0 +1,17 @@
+package grades;
+
+import java.util.ArrayList;
+
+public class Student {
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public Student(String name) {
+        this.name = name;
+        this.grades = new ArrayList<>();
+    }
+
+    public String getName(){
+        return this.name;
+    }
+}
