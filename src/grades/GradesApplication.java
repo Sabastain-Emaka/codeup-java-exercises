8854d57b4710c7091deae4240a0714e30d46f8fa
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-11 13:54:35 -0600

    working on student grades exe

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,5 +1,30 @@
 package grades;
 
+import util.Input;
+
+import java.util.HashMap;
+
 public class GradesApplication {
+
+    static HashMap<String, Student> students = new HashMap<>();
+    static Input input = new Input();
+
+    public static void main(String[] args) {
+        Student entoi = new Student("Ekoh");
+        entoi.addGrade(100);
+        entoi.addGrade(100);
+
+       Student  mclato = new Student("Mbabo");
+        mclato.addGrade(100);
+        mclato.addGrade(80);
+
+        Student mwanneh= new Student("Mbave");
+        mwanneh.addGrade(100);
+        mwanneh.addGrade(70);
+
+        Student entoh= new Student("Ekombo");
+        entoh.addGrade(100);
+        entoh.addGrade(100);
+    }
 
 }
