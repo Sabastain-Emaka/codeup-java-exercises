c2458946dbc1ab0a0afae6fc441b84505e297bfb
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-11 12:24:13 -0600

    working on student grades exercise

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -10,15 +10,16 @@ public class Student {
         this.name = name;
         this.grades = new ArrayList<>();
     }
-
+ // returns the student's name
     public String getName(){
         return this.name;
     }
-
+ //// adds the given grade to the grades property
     public void addGrade(int grade){
         this.grades.add(grade);
     }
 
+    // // returns the average of the students grades
     public double getGradeAverage(){
         int sum = 0;
         for (int grade: this.grades) {
