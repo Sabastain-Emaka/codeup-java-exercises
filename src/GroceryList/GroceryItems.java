a0e2e5d3507293bd9580e60f9124cd98f0cb8734
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-11 22:17:42 -0600

    working on grocery list

--- src/GroceryList/GroceryItems.java
+++ src/GroceryList/GroceryItems.java
@@ -2,4 +2,12 @@ package GroceryList;
 
 public class GroceryItems {
 
+    private String name;
+    private int amount;
+
+    public GroceryItems(String name, int amount){
+        this.name = name;
+        this.amount = amount;
+    }
+
 }
