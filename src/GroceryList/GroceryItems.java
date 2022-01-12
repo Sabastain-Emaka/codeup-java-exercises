f2d45d9b9f721c6c7c35c1d9d19f6a69c205f2d2
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-11 22:20:29 -0600

    grocery list

--- src/GroceryList/GroceryItems.java
+++ src/GroceryList/GroceryItems.java
@@ -10,4 +10,23 @@ public class GroceryItems {
         this.amount = amount;
     }
 
+        public String getName() {
+            return this.name;
+        }
+
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+
+        public int getAmount() {
+            return this.amount;
+        }
+
+
+        public void setAmount(int amount) {
+            this.amount = amount;
+        }
+
 }
