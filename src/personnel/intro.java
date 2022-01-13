01dfe4ec9dcf59f52413d48526ac11ee0242f69f
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-12 20:25:47 -0600

    peronnel

--- src/personnel/intro.java
+++ src/personnel/intro.java
@@ -3,7 +3,28 @@ package personnel;
 public class intro {
 
     public static void main(String[] args){
+
+        // before we declare a variable, we think of the data type we want to assign
+        // to the variable is it ;
+        // an integer abbreviated int
+        // is a String. String uses " ", with the value inside the quotes
+
+        // int x; // declaration of a variable
+       // x = 123 // is called assignment
+        //int x = 123; // this is initialization
+      int x = 123;
+       System.out.println(x);
+
+        // long data type
+        long K = 1234567890123456789L;
+
+        // byte
+
+        byte g = 12;
+        System.out.println("My number is: " +g);
+
         System.out.println("\\\t\"good to start\"");
         System.out.println("good to start\n");
     }
+
 }
