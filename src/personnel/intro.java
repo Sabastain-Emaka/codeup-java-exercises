0810eb214bc4ec9d7087f445f83cc4c0fe5dd9f6
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-12 19:57:22 -0600

    personnel

---
+++ src/personnel/intro.java
@@ -0,0 +1,9 @@
+package personnel;
+
+public class intro {
+
+    public static void main(String[] args){
+        System.out.println("\\\t\"good to start\"");
+        System.out.println("good to start\n");
+    }
+}
